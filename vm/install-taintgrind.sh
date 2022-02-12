0bc3318abc5a23acc48b245904295c8a0bec834a
Hugo Pacheco <hpacheco@gmail.com>
2022-02-12 13:46:09 +0000

    taintgrind

--- vm/install-taintgrind.sh
+++ vm/install-taintgrind.sh
@@ -2,12 +2,21 @@
 
 INST_DIR=$PWD
 
+# build valgrind
 curl https://sourceware.org/pub/valgrind/valgrind-3.18.1.tar.bz2 | tar xj
 cd valgrind-3.18.1
 ./autogen.sh
 ./configure --prefix=$INST_DIR
 make -j $(nproc)
 make install
+
+# build capstone
+cd taintgrind && \
+    wget https://github.com/capstone-engine/capstone/archive/refs/tags/4.0.2.tar.gz -O capstone.tar.gz && \
+    tar xf capstone.tar.gz && \
+    sh configure_capstone.sh `pwd`/../build && \
+    cd capstone-4.0.2 && \
+    sh make_capstone.sh
 
 git clone --depth 1 https://github.com/wmkhoo/taintgrind.git
 cd taintgrind
@@ -22,3 +31,4 @@ for i in *
 do
 	mv $i taintgrind-$i
 done
+	
