b395a1a6c0c611bb014dce8c877db666a90eb345
kali <kali@bad.localdomain>
2022-02-22 23:15:47 +0000

    tools

---
+++ vm/install-web.sh
@@ -0,0 +1,9 @@
+sudo apt install -y zaproxy niktos npm
+pip install semgrep
+echo 'export PATH=$PATH:/home/kali/.local/bin' >> ~/.profile
+source ~/.profile
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo npm install -g snyk snyk-to-html
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
--- vm/install.sh
+++ vm/install.sh
@@ -1,5 +1,14 @@
 #/bin/sh
 
+# add ubuntu repositories for older packages
+sudo apt install -y apt-add-repository
+sudo add-apt-repository 'deb http://cz.archive.ubuntu.com/ubuntu focal main universe'
+sudo add-apt-repository 'deb http://security.ubuntu.com/ubuntu focal-security main'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32 871920D1991BC93C
+
+sudo cp ubuntu.prefs /etc/apt/preferences.d/ubuntu
+sudo chown root /etc/apt/preferences.d/ubuntu.prefs
+
 sudo apt update
 sudo apt install -y curl wget gedit vim pkg-config cmake autotools-dev automake docker docker.io git
 sudo sudo apt-get install -y gcc g++ cmake libgmp-dev libboost-dev libboost-filesystem-dev \
@@ -7,8 +16,9 @@ sudo sudo apt-get install -y gcc g++ cmake libgmp-dev libboost-dev libboost-file
     libz-dev libedit-dev llvm-9 llvm-9-dev llvm-9-tools clang-9
 sudo apt install -y clang clang-tools valgrind
 sudo apt install -y libmysql++-dev libpam0g-dev libmysqlcppconn-dev
-sudo apt install -y python3-distutils 
+sudo apt install -y python3-distutils
 sh install-taintgrind.sh
 sh install-ikos.sh
 sh install-frama-c.sh
 sh install-timecop.sh
+sh install-web.sh
