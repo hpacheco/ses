4c8196f2c9b50553ecb4e5145be31115973b2ade
Hugo Pacheco <hpacheco@gmail.com>
2024-02-14 21:48:55 +0000

    Update install.sh

--- vm/install.sh
+++ vm/install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install -yq software-properties-common
-sudo apt install -yq curl wget gedit vim pkg-config cmake autotools-dev automake docker docker.io git
+sudo apt install -yq curl wget gedit vim pkg-config cmake autotools-dev automake docker.io git
 sudo sudo apt-get install -y gcc g++ cmake libgmp-dev libboost-dev libboost-filesystem-dev \
     libboost-thread-dev libboost-test-dev python3 python3-pygments libsqlite3-dev libtbb-dev \
     libz-dev libedit-dev gcc-multilib
